15734e67c920ede27c9bb2bd98c438689dc74978
lubowsky <lubowsky81@yandex.ru>
2025-12-18 18:32:16 +0700

    Environment Variables

--- miniapp/src/api/entriesApi.ts
+++ miniapp/src/api/entriesApi.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { MoodEntry } from "../types/MoodEntry";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 export async function fetchEntries(telegramId: number): Promise<MoodEntry[]> {
-  const API_URL = import.meta.env.VITE_API_URL;
+  
   const res = await axios.get(
     `${API_URL}/entries?telegramId=${telegramId}`
   );
