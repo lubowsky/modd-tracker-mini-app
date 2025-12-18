bf63284d8b3e2abe330f790bb89a86759bcc7766
lubowsky <lubowsky81@yandex.ru>
2025-12-18 16:59:34 +0700

    chore: add vite env typings

--- miniapp/src/api/entriesApi.ts
+++ miniapp/src/api/entriesApi.ts
@@ -2,8 +2,9 @@ import axios from "axios";
 import { MoodEntry } from "../types/MoodEntry";
 
 export async function fetchEntries(telegramId: number): Promise<MoodEntry[]> {
+  const API_URL = import.meta.env.VITE_API_URL;
   const res = await axios.get(
-    `http://localhost:3001/entries?telegramId=${telegramId}`
+    `${API_URL}/entries?telegramId=${telegramId}`
   );
   return res.data.entries;
 }
